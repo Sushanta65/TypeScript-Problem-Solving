8b0b3acaaca0e9eab09847e33ff146c21d31a883
Sushanta65 <sushanta9119@gmail.com>
2025-05-08 01:19:48 +0600

    Problem 5 Solved

--- main.ts
+++ main.ts
@@ -64,6 +64,16 @@ class Car extends Vehicle{
     }
 }
 
+// Problem 5
+
+function processValue(value: string | number): number{
+    if(typeof value === 'string'){
+        return value.length;
+    }else{
+        return value * 2;
+    }
+}
+
 
 
 
