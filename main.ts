0a6aaf3a3c8ef5b79cd516459444b2d832bc28db
Sushanta65 <sushanta9119@gmail.com>
2025-05-08 01:01:10 +0600

    Problem-2 Solved

--- main.ts
+++ main.ts
@@ -6,3 +6,17 @@ function formatString(input: string, toUpper: boolean  = true) : string {
 }
 
 // console.log(formatString('hello'))
+
+
+// Problem 2
+const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+  ];
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
+    return items.filter(item => item.rating >= 4)
+}
+
+// console.log(filterByRating(books))
